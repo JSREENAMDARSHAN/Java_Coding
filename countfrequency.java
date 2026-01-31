d7719f8cbddfc48a024b247c5f6dce3be03b2267
Sreenam Darshan <2369973@cognizant.com>
2026-01-31 19:23:05 +0530

    added new code

---
+++ countfrequency.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class countfrequency {
+    public static void main(String[] args){
+        String str = "abbba";
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i = 0;i<str.length();i++){
+            char ch = str.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        System.out.println(map);
+        
+    }
+}
