53b0145fbe0be93ddfe1afcb7d68ec2eeba050d9
Sreenam Darshan <2369973@cognizant.com>
2026-01-31 19:48:34 +0530

    added new code

---
+++ mergetwosortedarray.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class mergetwosortedarray{
+    public static void main(String[] args) {
+        int [] arr1 = {1,2,3};
+        int [] arr2 = {2,4,5,6};
+        int [] arr3 = new int[arr1.length+arr2.length];
+        int i = 0,j=0,k=0;
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]<=arr2[j]){
+                arr3[k] = arr1[i];
+                i++;
+                k++; 
+            }
+            else{
+                arr3[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+        while(i<arr1.length){
+            arr3[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while(j<arr2.length){
+            arr3[k] = arr2[j];
+            j++;
+            k++;
+        }
+        System.out.println(Arrays.toString(arr3));
+    }
+}
