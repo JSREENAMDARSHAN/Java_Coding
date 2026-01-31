bf6330269e4ac00edafd45944e764da3b3088605
Sreenam Darshan <2369973@cognizant.com>
2026-01-31 10:01:46 +0530

    StringBuilder

--- Reverse_string.java
+++ Reverse_string.java
@@ -2,10 +2,10 @@ public class Reverse_string{
 
     public static void main(String[] args) {
         String str = "darshan";
-        String nstr = "";
+        StringBuilder nstr = new StringBuilder();
         int length = str.length();
         for(int i = length - 1 ; i>=0 ; i--){
-            nstr = nstr + str.charAt(i);
+            nstr.append(str.charAt(i));
         }
         System.out.println("Original String: " + str);
         System.out.println("Reveresed String: " + nstr);
